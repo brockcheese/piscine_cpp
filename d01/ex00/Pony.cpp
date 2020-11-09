e5a36e59619b73ae5de337266d2bfc2c0492df80
Armando Sanchez <ArmandoSanchez@Armandos-MBP.cable.rcn.com>
2020-11-08 19:52:39 -0600

    allocate on stack vs heap

--- d01/ex00/Pony.cpp
+++ d01/ex00/Pony.cpp
@@ -1,42 +1,51 @@
 #include "Pony.hpp"
 
+//default constructor
+
 Pony::Pony() :
 	_name("Default Pony"),
 	_age(0),
 	_nblegs(4)
-{
+{ //print pony data
 	std::cout <<
 		"Neigh, who goes there? I am " << _name <<
 		", a " << _age <<
 		" year old pony with " << _nblegs <<
 		" remaining legs left." << std::endl;
-	return;
+	return; //end function
 }
+
+//constructor
 
 Pony::Pony(std::string name, int age, int nblegs) :
 	_name(name),
 	_age(age),
 	_nblegs(nblegs)
-{
+{ //print pony data
 	std::cout <<
 		"Neigh, who goes there? I am " << _name <<
 		" a " << _age <<
 		" year old pony with " << _nblegs <<
 		" remaining legs left." << std::endl;
-	return;
+	return; //end function
 }
 
-Pony::~Pony() {
+//default destructor
+
+Pony::~Pony() { //prints destructor message
 	puts("Why have you forsaken me? You know ponies don't go to heaven.");
-	return;
+	return; //end function
 }
+
+//remove one of the pony's legs
 
 void Pony::removeLeg() {
-	if (_nblegs > 0)
+	if (_nblegs > 0) //if pony has legs remove leg
 	{
 		puts("Removing Leg:");
 		_nblegs--;
 	}
+	//print pony statement depending on number of legs left
 	if (_nblegs == 3)
 		puts("Tis but a scratch");
 	if (_nblegs == 2)
@@ -47,14 +56,20 @@ void Pony::removeLeg() {
 		puts("I'll bite your legs off!");
 }
 
+//returns the name of a pony
+
 std::string Pony::getName() {
-	return (_name);
+	return (_name); //return pony name
 }
+
+//returns the age of a pony
 
 int Pony::getAge() {
-	return (_age);
+	return (_age); //return pony age
 }
 
+//returns the number of legs on a pony
+
 int Pony::getnbLegs() {
-	return (_nblegs);
+	return (_nblegs); //return number of legs on a pony
 }
--- d01/ex00/main.cpp
+++ d01/ex00/main.cpp
@@ -1,23 +1,30 @@
 #include "Pony.hpp"
 
+//creates a pony on the heap
+
 static void ponyOnTheHeap() {
+	//allocates chonk to store a new pony on the heap
 	Pony *chonk = new Pony("Chonky Boi", 37, 3);
-	delete(chonk);
+	delete(chonk); //frees memory allocates by chonk
 }
+
+//creates a pony on the stack
 
 static void ponyOnTheStack() {
-	Pony  blackKnight;
+	Pony  blackKnight; //allocates pony on the stack
 
-	blackKnight.removeLeg();
+	blackKnight.removeLeg(); //remove ponie's leg
 	blackKnight.removeLeg();
 	blackKnight.removeLeg();
 	blackKnight.removeLeg();
 }
+
+//demonstrates allocating memory on a heap vs on a stack
 
 int	main() {
 	puts("_____Pony on the Heap Doodleoo Doo Doo_____");
-	ponyOnTheHeap();
+	ponyOnTheHeap(); //creates a pony on the heap
 	puts("_____Pony on the Stack______");
-	ponyOnTheStack();
-	return (0);
+	ponyOnTheStack(); //creates a pony on the stack
+	return (0); //ends program
 }
