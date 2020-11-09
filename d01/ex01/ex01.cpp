4b5121d1b881bb2684a84f6c1445e98374cdfa41
Armando Sanchez <ArmandoSanchez@Armandos-MBP.cable.rcn.com>
2020-11-08 19:59:30 -0600

    fixing memory leak

--- d01/ex01/ex01.cpp
+++ d01/ex01/ex01.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
+//fixes a memory leak
+
 void	memoryLeak()
 {
+	//allocates memory for a string
 	std::string*	panthere = new std::string("String panthere");
 
-	std::cout << *panthere << std::endl;
-	delete(panthere);
+	std::cout << *panthere << std::endl; //prints string
+	delete(panthere); //frees memory previously allocated
 }
