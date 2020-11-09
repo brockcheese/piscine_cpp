395cce35cd5a1aee48f27706f02fc01f170925a9
Armando Sanchez <ArmandoSanchez@Armandos-MBP.cable.rcn.com>
2020-11-08 20:35:52 -0600

    pointer vs reference

--- d01/ex04/ex04.cpp
+++ d01/ex04/ex04.cpp
@@ -1,10 +1,13 @@
 #include <string>
 #include <iostream>
 
-int	main() {
-	std::string s = "HI THIS IS BRAIN";
-	std::string* p = &s;
-	std::string& r = s;
+//program in which a string is created along with a pointer and reference to it
 
+int	main() {
+	std::string s = "HI THIS IS BRAIN"; //creates string
+	std::string* p = &s; //creates pointer to string
+	std::string& r = s; //creates reference to string
+
+	//prints pointer and reference to string
 	std::cout << *p << std::endl << r << std::endl;
 }
